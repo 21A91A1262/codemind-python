8f84f74a5a8c5a2d2234c63a3f22118c76eca774
21A91A1262 <97424601+21A91A1262@users.noreply.github.com>
2022-07-14 12:17:20 +0530

    n=input() n=n.lower() k=set(n) n1=input() n1=n1.lower() l=set(n1)
    if(k==l):
        print("True")
    else:
        print(False)

--- Anagrams.py
+++ Anagrams.py
@@ -1,18 +1,11 @@
+n=input()
+n=n.lower()
+k=set(n)
+n1=input()
+n1=n1.lower()
+l=set(n1)
 
-s=input()
-x=input()
-
-c=v=0
-if len(s)==len(x):
-    for i in x:
-        if i in s or i.upper() in s or i.lower() in s:
-            c+=1
-        else:
-            v=1
-            break
-    if(c==len(x) and v!=1):
-        print(True)
-    else:
-        print(False)
+if(k==l):
+    print("True")
 else:
     print(False)
